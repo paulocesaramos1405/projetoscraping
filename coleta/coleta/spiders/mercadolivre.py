f960a9dfb938403214c94155455507e114d8bbd1
Paulo César Ramos <rpaulocesar44@gmail.com>
2025-04-08 17:30:56 -0300

    commit2

---
+++ coleta/coleta/spiders/mercadolivre.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class MercadolivreSpider(scrapy.Spider):
+    name = "mercadolivre"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/capacete?sb=rb#D[A:capacete]"]
+
+    def parse(self, response):
+        pass
